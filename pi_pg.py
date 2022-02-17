7ef5ef227eb67e7989d06d21ddaaf658cc7427b9
Grigoriy32 <98540737+Grigoriy32@users.noreply.github.com>
2022-02-17 20:20:59 +0400

    Update pi_pg.py

--- pi_pg.py
+++ pi_pg.py
@@ -4,6 +4,9 @@ mixer.pre_init(44100, -16, 1, 512)
 init()
 font.init()
 
+counter1 = 0
+counter2 = 0
+
 x1 = 0
 y1 = 100
 #создай окно игры
@@ -14,11 +17,14 @@ display.set_caption("Пинг-Понг")
 #задай фон сцены
 background = transform.scale(image.load('zadniy_phone.png'), (700, 500))
 FPS = 60
+font1 = font.Font(None, 35)
+loose1 = font1.render('PLAYER 1 LOOSE!', True, (150, 20, 0))
+loose2 = font1.render('PLAYER 2 LOOSE!', True, (150, 20, 0))
 
 clock = time.Clock()
 score = 0
 game = True
-
+finish = False
 class GameSprite(sprite.Sprite):
    #конструктор класса
    def __init__(self, player_image, player_x, player_y, player_speed, size_x, size_y):
@@ -49,16 +55,42 @@ class Player(GameSprite):
 
 rocket1 = Player('rocket.jpg', x1, y1, 10, 20, 100)
 rocket2 = Player('rocket.jpg',680, y1, 10, 20, 100)
-
-
+ball = Player('ball.png', 350, 250, 15, 50, 50)
+ball_speed_x = 3
+ball_speed_y = 3
 while game:
     for e in event.get():
         if e.type == QUIT:
            game = False
+    first = font1.render('У первого игрока:' + str(counter1), True, (170, 50, 50))
+    second = font1.render('У второго игрока:' + str(counter2), True, (170, 50, 50))
     window.blit(background, (0,0))
+    window.blit(first, (0, 20))
+    window.blit(second, (0, 55))
     rocket1.reset()
     rocket2.reset()
+    ball.reset()
     rocket1.update_l()
     rocket2.update_r()
+    if finish != True:
+        ball.rect.x += ball_speed_x
+        ball.rect.y += ball_speed_y
+    if ball.rect.y > height_win-50:
+        ball_speed_y *= -1
+    elif ball.rect.y < 0:
+        ball_speed_y *= -1
+    if sprite.collide_rect(rocket1, ball):
+        ball_speed_x *= -1
+        counter1 += 1
+    elif sprite.collide_rect(rocket2, ball):
+        ball_speed_x *= -1
+        counter2 += 1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(loose1, (250,250))
+    elif ball.rect.x > width_win:
+        finish = True
+        window.blit(loose2, (250,250))
+    
     display.update()
     clock.tick(FPS)
